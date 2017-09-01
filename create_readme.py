d272afba830a45e3c69ec3e79a9a8c8084aa6f3f
neargle <nearg1e.com@gmail.com>
2017-09-01 16:15:27 +0800

    "update readme.md"

--- create_readme.py
+++ create_readme.py
@@ -1,10 +1,11 @@
 '''Generate the root Readme file'''
 import os
 import re
+import subprocess
 
 re_md_h1 = re.compile("^#[^#](.*)", re.I)
 readme_markdown = '''
-# 平时的一些小实验，小代码，小tip的收集
+# 平时做的一些小实验，小代码，小Tip的收集和分享
 
 平时做的一些如fuzz的小实验，或者写的一些小工具，其内容并不能成为一篇文章或者一个项目，但是又是可以分享的，我就放到这里来。
 
@@ -39,6 +40,8 @@ def main():
     root_readme = readme_markdown.format(tree=tree)
     with open("readme.md", "w") as readme:
         readme.write(root_readme)
+    git_commit_lst = ['git', 'commit', '-am', '"update readme.md"']
+    subprocess.Popen(git_commit_lst)
 
 if __name__ == '__main__':
     main()
