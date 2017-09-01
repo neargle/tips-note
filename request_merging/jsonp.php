4314ea860a0cf440fec2e0d5a5871d5e0d6fb64f
neargle <nearg1e.com@gmail.com>
2017-09-01 16:41:54 +0800

    www

--- request_merging/jsonp.php
+++ request_merging/jsonp.php
@@ -7,7 +7,7 @@ function startsWith($url, $domain) {
 
 $referrer = @$_SERVER['HTTP_REFERER'];
 
-if (startsWith($referrer, "http://10.101.122.75:8082")) {
+if (startsWith($referrer, "http://example.com:8082")) {
     $js_code = 'function jquery() { return "security content";}';
     echo $js_code;
 } else {
